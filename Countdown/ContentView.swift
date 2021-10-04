099c7e851ab5303c52e12aeeb4e132a6053f0ea4
Pedro <pedrocpt2001@gmail.com>
2021-10-03 23:38:27 -0400

    Initial Commit

---
+++ Countdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Countdown
+//
+//  Created by Pedro Pagán on 10/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Pedro Pagán on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
