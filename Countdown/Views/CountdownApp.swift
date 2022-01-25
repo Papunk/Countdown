980a3c937abcfb2f7318e8c018069aa5b9facb50
Pedro C. Pagán Torres <pedrocpt2001@gmail.com>
2022-01-24 23:29:24 -0400

    Added sidebar; added debug button

--- Countdown/Views/CountdownApp.swift
+++ Countdown/Views/CountdownApp.swift
@@ -9,29 +9,41 @@ import SwiftUI
 
 @main
 struct CountdownApp: App {
-//    @State private var window: NSWindow?
+    
+    @State var timerList = [TimerModel]()
+    @State var addingTimer = false
 
     var body: some Scene {
         WindowGroup {
-            ListView()
-//                .navigationTitle("Hello")
-//                .background(WindowAccessor(window: $window).setUp())
-                .onReceive(NotificationCenter.default.publisher(for: NSApplication.willUpdateNotification), perform: { _ in
-                    for window in NSApplication.shared.windows {
-                        window.standardWindowButton(.zoomButton)?.isEnabled = false
+            NavigationView {
+                ListView(timerList: $timerList)
+                    .frame(minWidth: 200)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .automatic) {
+                    Button(action: {
+                        for i in 0...5 { timerList.append(TimerModel("Test\(i)", h: UInt8(i), m: 0, s: 0)) }
+                    }, label: {
+                        Image(systemName: "ladybug")
+                    })
+                    Button(action: {
+                        addingTimer.toggle()
+                    }, label: {
+                        Image(systemName: "plus")
+                    })
+                    .sheet(isPresented: $addingTimer, onDismiss: {}) {
+                        NewTimerDialog(isShown: $addingTimer, timerList: $timerList)
                     }
-                })
-                .frame(minWidth: 400, idealWidth: 400, minHeight: 300, idealHeight: 300)
+                    .keyboardShortcut("n")
+                }
+            }
         }
-        .commands { CommandGroup(replacing: .newItem, addition: { }) }
-//        WindowGroup("Timer") {
-//            ForEach(handler.timerList) { timer in
-//                if !timer.paused {
-//                    TimerView(timer: timer) // this may need to be a binding
-//                }
-//            }
-//        }
-//        .handlesExternalEvents(matching: Set(arrayLiteral: "OtherWindow"))
+        .commands {
+            CommandGroup(replacing: .newItem, addition: { })
+            SidebarCommands()
+        }
+        
+        
         Settings {
             VStack {
                 Text("Under construction")
@@ -42,22 +54,3 @@ struct CountdownApp: App {
         }
     }
 }
-
-//struct WindowAccessor: NSViewRepresentable {
-//    @Binding var window: NSWindow?
-//
-//    func makeNSView(context: Context) -> NSView {
-//        let view = NSView()
-//        DispatchQueue.main.async {
-//            self.window = view.window
-//        }
-//        return view
-//    }
-//
-//    func updateNSView(_ nsView: NSView, context: Context) {}
-//
-//    func setUp() -> WindowAccessor {
-//        window?.backgroundColor = NSColor(red: 0, green: 0, blue: 0, alpha: 0.8)
-//        return self
-//    }
-//}
--- Countdown/Views/ListView.swift
+++ Countdown/Views/ListView.swift
@@ -9,54 +9,28 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var addingTimer = false
-    @State var timerList: [TimerModel] = []
+    @Binding var timerList: [TimerModel]
     
     var body: some View {
+
         List {
             ForEach(timerList) { timer in
                 HStack {
-                    Text(timer.title)
-                    Spacer()
-                    Text(timer.format())
-                    Button(action: {
-                        timer.paused.toggle()
-                    }, label: {
-                        Image(systemName: timer.paused ? "play.fill" : "pause.fill")
-                    })
                     NavigationLink(destination: TimerView(timer: timer)) {
-                        Image(systemName: "rectangle.on.rectangle")
+                        Text(timer.title)
+                        Spacer()
+                        Text(timer.format())
                     }
-//                    Button(action: {
-//
-//                    }, label: {
-//                    })
                 }
                 Divider()
             }
         }
-        .toolbar {
-            ToolbarItemGroup(placement: .automatic) {
-                Button(action: {}) {
-                    Image(systemName: "pencil")
-                }
-                Button(action: {}) {
-                    Image(systemName: "stop.circle")
-                }
-                Button(action: { addingTimer.toggle() }) {
-                    Image(systemName: "plus")
-                }
-                .sheet(isPresented: $addingTimer, onDismiss: {}) {
-                    NewTimerDialog(isShown: $addingTimer, timerList: $timerList)
-                }
-                .keyboardShortcut("n")
-            }
-        }
+        .listStyle(SidebarListStyle())
     }
 }
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
+        ListView(timerList: .constant([]))
     }
 }
