ca6a573189bbbad0be80941247c00590523abdeb
Pedro <pedrocpt2001@icloud.com>
2022-02-03 23:42:19 -0400

    Optimized Timer publisher. Instead of using one per timer, a single master clock is used. Timers now update in real time in the sidebar.

--- Countdown/Views/ListView.swift
+++ Countdown/Views/ListView.swift
@@ -10,21 +10,26 @@ import SwiftUI
 struct ListView: View {
     
     @Binding var timerList: [TimerModel]
-    
+    @State var masterClock = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State var x = 0
+
     var body: some View {
-        List {
-            ForEach(timerList) { timer in
-                HStack {
-                    NavigationLink(destination: TimerView(timer: timer)) {
-                        Text(timer.title)
-                        Spacer()
-                        Text(timer.format())
+        VStack {
+            Text("Clock is working: \(x)s").onReceive(masterClock, perform: { _ in x += 1 })
+            List {
+                ForEach(timerList) { timer in
+                    HStack {
+                        NavigationLink(destination: TimerView(timer: timer, clock: $masterClock)) {
+                            Text(timer.title)
+                            Spacer()
+                            Text(timer.format())
+                        }
                     }
+                    .padding([.top, .bottom], 10)
                 }
-                .padding([.top, .bottom], 10)
             }
+            .listStyle(SidebarListStyle())
         }
-        .listStyle(SidebarListStyle())
     }
 }
 
--- Countdown/Views/TimerView.swift
+++ Countdown/Views/TimerView.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import Combine
 
 struct TimerView: View {
     @ObservedObject var timer: TimerModel
-    let clock = Timer.publish(every: 1, on: .main, in: .common).autoconnect() // Consider having a single one of these
+    @Binding var clock: Publishers.Autoconnect<Timer.TimerPublisher>
 
     var body: some View {
         VStack {
@@ -37,6 +38,6 @@ struct TimerView: View {
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
-        TimerView(timer: TimerModel("Computer Time", h: 5, m: 30, s: 10))
+        TimerView(timer: TimerModel("Computer Time", h: 5, m: 30, s: 10), clock: .constant(Timer.publish(every: 1, on: .main, in: .common).autoconnect()))
     }
 }
